f9a81e42f83ae5f8d10c101c84ce6f1757700358
brynbeaudry <babeaudry@gmail.com>
2017-08-01 01:17:59 -0400

    add axios, manual added yarn files, register paper is centered

--- src/routes/Register/components/Register.js
+++ src/routes/Register/components/Register.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Paper from 'material-ui/Paper';
-import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
-import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper'
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator'
+import RaisedButton from 'material-ui/RaisedButton'
 
-/*The Register Form*/
+/* The Register Form */
 /*
 First name
 Last Name
@@ -25,119 +25,124 @@ Handlesumbit should be the dispatch function register user passed in as props
 */
 const paperStyle = {
   height: 'auto',
-  width: '5em',
-  margin: 'auto',
+  width: 'auto',
+  margin: '1em',
   display: 'block',
-};
+  textAlign : 'center',
+}
+
+const buttonStyle = {
+
+}
 
 class Register extends Component {
-  constructor(props) {
-        super(props);
-        this.state = {
-          user: {
-            first_name : null,
-            last_name : null,
-            email : null,
-            password : null,
-          },
-          formIsValid : false,
-        }
-        this.handleChange = this.handleChange.bind(this);
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: {
+        first_name : null,
+        last_name : null,
+        email : null,
+        password : null,
+      },
+      formIsValid : false,
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-    componentWillMount() {
-        // custom rule will have name 'isPasswordMatch'
-        ValidatorForm.addValidationRule('isPasswordMatch', (value) => {
-            if (value !== this.state.user.password) {
-                return false;
-            }
-            return true;
-        });
-    }
+  componentWillMount () {
+    // custom rule will have name 'isPasswordMatch'
+    ValidatorForm.addValidationRule('isPasswordMatch', (value) => {
+      if (value !== this.state.user.password) {
+        return false
+      }
+      return true
+    })
+  }
 
-    handleChange(event) {
-        const { user } = this.state;
-        user[event.target.name] = event.target.value;
-        this.setState({ user });
-    }
+  handleChange (event) {
+    const { user } = this.state
+    user[event.target.name] = event.target.value
+    this.setState({ user })
+  }
 
-    //handleSubmit is called when the form is valid automatically
-    handleSubmit(){
-      //
-      this.setState({...this.state, formIsValid:true})
-    }
+  // handleSubmit is called when the form is valid automatically
+  handleSubmit () {
+    //
+    this.setState({ ...this.state, formIsValid:true })
+  }
 
-    onSumbit(){
-      this.props.registerUser();
-    }
+  onSumbit () {
+    this.props.registerUser()
+  }
 
-  render(){
+  render () {
     return (
-          <Paper
-            //style={paperStyle} 
-            zDepth={3}>
-            <ValidatorForm
-              ref="form"
-              onSubmit={this.handleSubmit}
-              onError={errors => console.log(errors)}
-            >
-              <TextValidator
-                hintText="First Name"
-                //errorText={this.props.errors.register.first_name}
-                floatingLabelText="First Name"
-                multiLine={false}
-                name='first_name'
-                id='first_name'
-                validators={['required']}
-                errorMessages={['First name is required']}
-                onChange = {this.handleChange}
-              /><br />
-              <TextValidator
-                hintText="Last Name"
-                //errorText={this.props.errors.register.last_name}
-                floatingLabelText="Last Name"
-                multiLine={false}
-                name='last_name'
-                validators={['required']}
-                errorMessages={['Last name is required']}
-                onChange = {this.handleChange}
-              /><br />
-              <TextValidator
-                hintText="Email"
-                //errorText={this.props.errors.register.email}
-                floatingLabelText="Email"
-                multiLine={false}
-                name='email'
-                validators={['required', 'isEmail']}
-                errorMessages={['Email is required', 'Please enter a valid email']}
-                onChange = {this.handleChange}
-              /><br />
-              <TextValidator
-                hintText="Password"
-                //errorText={this.props.errors.register.password}
-                floatingLabelText="Password"
-                multiLine={false}
-                name='password'
-                type='password'
-                validators={['required']}
-                errorMessages={['Password is required']}
-                onChange = {this.handleChange}
-              /><br />
-              <TextValidator
-                hintText="Confirm Password"
-                //errorText={this.props.errors.register.confirm_password}
-                floatingLabelText="Confirm Password"
-                multiLine={false}
-                name='confirm_password'
-                validators={['required','isPasswordMatch']}
-                errorMessages={['Please confirm your password', 'Passwords do not match']}
-                onChange = {this.handleChange}
-              /><br />
-              <RaisedButton label="Primary" primary={true} //style={style}
-              disabled={this.state.formIsValid ? true: false} onClick={this.onSubmit}/>
-            </ValidatorForm>
-          </Paper>
-    );
+      <Paper
+        style={paperStyle}
+        zDepth={3}>
+        <ValidatorForm
+          ref='form'
+          onSubmit={this.handleSubmit}
+          onError={errors => console.log(errors)}
+        >
+          <TextValidator
+            hintText='First Name'
+            // errorText={this.props.errors.register.first_name}
+            floatingLabelText='First Name'
+            multiLine={false}
+            name='first_name'
+            id='first_name'
+            validators={['required']}
+            errorMessages={['First name is required']}
+            onChange={this.handleChange}
+          /><br />
+          <TextValidator
+            hintText='Last Name'
+            // errorText={this.props.errors.register.last_name}
+            floatingLabelText='Last Name'
+            multiLine={false}
+            name='last_name'
+            validators={['required']}
+            errorMessages={['Last name is required']}
+            onChange={this.handleChange}
+          /><br />
+          <TextValidator
+            hintText='Email'
+            // errorText={this.props.errors.register.email}
+            floatingLabelText='Email'
+            multiLine={false}
+            name='email'
+            validators={['required', 'isEmail']}
+            errorMessages={['Email is required', 'Please enter a valid email']}
+            onChange={this.handleChange}
+          /><br />
+          <TextValidator
+            hintText='Password'
+            // errorText={this.props.errors.register.password}
+            floatingLabelText='Password'
+            multiLine={false}
+            name='password'
+            type='password'
+            validators={['required']}
+            errorMessages={['Password is required']}
+            onChange={this.handleChange}
+          /><br />
+          <TextValidator
+            hintText='Confirm Password'
+            // errorText={this.props.errors.register.confirm_password}
+            floatingLabelText='Confirm Password'
+            multiLine={false}
+            name='confirm_password'
+            validators={['required', 'isPasswordMatch']}
+            errorMessages={['Please confirm your password', 'Passwords do not match']}
+            onChange={this.handleChange}
+          /><br />
+          <RaisedButton label='Register' primary style={buttonStyle}
+            disabled={!!this.state.formIsValid} onClick={this.onSubmit} />
+        </ValidatorForm>
+      </Paper>
+    )
   }
 }
 Register.propTypes = {
